f2062e81edd905eb2c4cc0169117ba3e40f03ab6
Leroux Romain <romain.leroux@streamroot.io>
2019-01-22 20:17:59 +0100

    card for RingBitset and starting Recloserstate

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 
+enum RecloserState {
+    Open(RingBitSet),
+    HalfOpen(RingBitSet),
+    Closed(RingBitSet),
+}
+
+impl RecloserState {
+    pub fn call_permitted(&self) -> bool {
+        match *self {
+            RecloserState::Closed(_) => true,
+            _ => true,
+        }
+    }
+}
+
 #[derive(Debug)]
-pub struct RingBitSet {
+struct RingBitSet {
     len: usize,
+    card: AtomicUsize,
     ring: Box<[AtomicBool]>,
     index: AtomicUsize,
 }
@@ -17,22 +33,40 @@ impl RingBitSet {
 
         Self {
             len: len,
+            card: AtomicUsize::new(0),
             ring: buf.into_boxed_slice(),
             index: AtomicUsize::new(0),
         }
     }
 
-    pub fn set_current(&mut self, b: bool) {
+    pub fn set_current(&mut self, val_new: bool) -> bool {
         loop {
             let i = self.index.load(Ordering::Acquire);
             let j = if i == self.len - 1 { 0 } else { i + 1 };
-            let v = self.ring[i].load(Ordering::Acquire);
+
+            let val_old = self.ring[i].load(Ordering::Acquire);
+
+            let card_old = self.card.load(Ordering::Acquire);
+            let card_new = card_old - to_int(val_old) + to_int(val_new);
+
             if self.index.compare_and_swap(i, j, Ordering::Release) == i
-                && self.ring[i].compare_and_swap(v, b, Ordering::Release) == v
+                && self.ring[i].compare_and_swap(val_old, val_new, Ordering::Release) == val_old
+                && self
+                    .card
+                    .compare_and_swap(card_old, card_new, Ordering::Release)
+                    == card_old
             {
-                break;
+                return val_old;
             }
         }
+    }
+}
+
+fn to_int(b: bool) -> usize {
+    if b {
+        1
+    } else {
+        0
     }
 }
 
