3006be597096eec612db3831aa2fa4f8b0027a09
Leroux Romain <leroux.rgd@gmail.com>
2019-06-16 21:39:59 +0200

    using criterion and rayon in benches

--- benches/recloser.rs
+++ benches/recloser.rs
@@ -1,69 +1,57 @@
-#![feature(test)]
-
-extern crate fake_clock;
-extern crate recloser;
-extern crate test;
-
-use std::sync::{Arc, Barrier};
-use std::thread;
 use std::time::Duration;
 
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use fake_clock::FakeClock;
+use num_cpus;
+use rayon::prelude::*;
 use recloser::{AnyError, Error, Recloser};
 
-#[bench]
-fn single_threaded(b: &mut test::Bencher) {
-    let recloser = Recloser::new(AnyError, 0.01, 10, 5, Duration::from_secs(1));
-    let mut n = 0;
-
-    b.iter(move || {
-        match recloser.call(|| dangerous_call(n)) {
-            Ok(_) => {}
-            Err(Error::Inner(_)) => {}
-            Err(_) => {}
-        }
-        sleep(1500);
-        n += 1;
-    })
-}
-
-#[bench]
-fn multi_threaded(b: &mut test::Bencher) {
-    let recloser = Arc::new(Recloser::new(AnyError, 0.01, 10, 5, Duration::from_secs(1)));
-    let nb_threads = 8;
-
-    b.iter(move || {
-        let mut handles = Vec::with_capacity(nb_threads);
-        let barrier = Arc::new(Barrier::new(nb_threads));
-
-        for n in 0..nb_threads {
-            let c = barrier.clone();
-            let recloser = recloser.clone();
-
-            handles.push(thread::spawn(move || {
-                c.wait();
-                let res = match recloser.call(|| dangerous_call(n)) {
-                    Ok(_) => true,
-                    Err(Error::Inner(_)) => false,
-                    Err(_) => false,
-                };
-                test::black_box(res);
-                sleep(1500);
-            }));
-        }
-
-        handles.into_iter().for_each(|it| it.join().unwrap());
-    })
-}
-
-fn dangerous_call(n: usize) -> Result<usize, usize> {
-    if n % 5 == 0 {
-        test::black_box(Err(n))
-    } else {
-        test::black_box(Ok(n))
-    }
-}
+const ITER_C: u64 = 10_000;
 
 fn sleep(time: u64) {
     FakeClock::advance_time(time);
 }
+
+fn dangerous_call(n: u64) -> Result<u64, u64> {
+    if n % 5 == 0 {
+        black_box(Err(n))
+    } else {
+        black_box(Ok(n))
+    }
+}
+
+fn single_threaded() {
+    let recloser = Recloser::new(AnyError, 0.01, 10, 5, Duration::from_secs(1));
+
+    (0..ITER_C).into_iter().for_each(|i| {
+        match recloser.call(|| dangerous_call(i)) {
+            Ok(_) => {}
+            Err(Error::Inner(_)) => {}
+            Err(_) => {}
+        };
+        sleep(1500);
+    });
+}
+
+fn multi_threaded() {
+    let recloser = Recloser::new(AnyError, 0.01, 10, 5, Duration::from_secs(1));
+
+    (0..ITER_C * num_cpus::get() as u64)
+        .into_par_iter()
+        .for_each(|i| {
+            match recloser.call(|| dangerous_call(i)) {
+                Ok(_) => {}
+                Err(Error::Inner(_)) => {}
+                Err(_) => {}
+            };
+            sleep(1500);
+        });
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("single_threaded", |b| b.iter(|| single_threaded()));
+    c.bench_function("multi_threaded", |b| b.iter(|| multi_threaded()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
