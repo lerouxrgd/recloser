533f5332dfba347736ad1294f5e0636863553a19
Leroux Romain <leroux.rgd@gmail.com>
2019-06-27 01:40:22 +0200

    expose async as a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-mod r#async;
 mod error;
 mod recloser;
 mod ring_buffer;
 
+pub mod r#async;
+
 pub use crate::error::{AnyError, Error, ErrorPredicate};
-pub use crate::r#async::{AsyncRecloser, RecloserFuture};
 pub use crate::recloser::{Recloser, RecloserBuilder};
